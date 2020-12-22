f3fecfabe0328215b9033a14405d3ee6756d25da
Ikeh Leonard <leoikeh99@gmail.com>
2020-12-22 10:11:11 +0100

    Added routes

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ if (process.env.NODE_ENV === "production") {
   );
 }
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("listening on port:" + PORT));
